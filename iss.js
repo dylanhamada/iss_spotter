e693e532e0313b668ae7479767f431cc40f9d142
dylanhamada <dylan.hamada@gmail.com>
2023-10-27 08:34:41 -0400

    Write fetchMyIP function
    - Write fetchMyIP function in iss.js
    - Write cases in fetchMyIP for request errors, wrong HTTP status codes, and successful request
    - Call fetchMyIP callback in index.js

---
+++ iss.js
@@ -0,0 +1,27 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
+const request = require("request");
+
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request("https://api.ipify.org?format=json", (err, resp, body) => {
+    if (err) return callback(err, null);
+
+    if (Response.statusCode !== 200) {
+      const msg = `Status Code ${resp.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    callback(null, JSON.parse(body).ip);
+  });
+};
+
+module.exports = { fetchMyIP };
